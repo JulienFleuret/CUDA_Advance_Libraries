7e2b1b7c8de29fc55fec86f751fbcf0cbd83332f
julien-richard-fleuret <julien.fleuret@gmail.com>
2024-01-31 17:13:44 -0500

    minor update in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,14 +5,14 @@ dvc pull models.dvc
 fi
 
 if [ ! -d "$PWD/third_party" ]; then
-    $(PWD)/scripts/download_libtorch.sh
-    $(PWD)/scripts/download_cuda.sh
-    $(PWD)/scripts/download_opencv.sh
+    $PWD/scripts/download_libtorch.sh
+    $PWD/scripts/download_cuda.sh
+    $PWD/scripts/download_opencv.sh
 elif [ ! -d "$PWD/third_party/libtorch" ]; then
-    $(PWD)/scripts/download_libtorch.sh
+    $PWD/scripts/download_libtorch.sh
 elif [ ! -d "$PWD/third_party/cuda" ]; then
-    $(PWD)/scripts/download_cuda.sh
+    $PWD/scripts/download_cuda.sh
 elif [ ! -d "$PWD/third_party/opencv" ]; then
-    $(PWD)/scripts/download_opencv.sh
+    $PWD/scripts/download_opencv.sh
 fi
 
