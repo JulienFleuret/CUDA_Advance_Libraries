e49f28a626a450ef75d208909258f25afe1da050
julien-richard-fleuret <julien.fleuret@gmail.com>
2024-02-01 10:01:22 -0500

    repositionning the qq argument for unzip to front

--- dockerfile
+++ dockerfile
@@ -3,7 +3,7 @@ FROM ubuntu:23.04
 WORKDIR /opt
 
 
-RUN apt update && apt install -yqq gcc g++ git wget bash unzip libxml2-dev cmake &>/dev/null
+RUN apt update && apt install -yqq gcc g++ git wget bash unzip libxml2-dev cmake
 RUN git clone https://github.com/JulienFleuret/CUDA_Advance_Libraries.git .
 COPY ./models /opt/models
 RUN echo $(ls)
--- scripts/download_libtorch.sh
+++ scripts/download_libtorch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir third_party
 wget https://download.pytorch.org/libtorch/cu121/libtorch-cxx11-abi-shared-with-deps-2.1.2%2Bcu121.zip -q
-unzip libtorch-cxx11-abi-shared-with-deps-2.1.2+cu121.zip -qq
+unzip -qq libtorch-cxx11-abi-shared-with-deps-2.1.2+cu121.zip
 rm libtorch-cxx11-abi-shared-with-deps-2.1.2+cu121.zip
 mv libtorch third_party/libtorch
 
